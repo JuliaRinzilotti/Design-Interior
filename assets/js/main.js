5dc39c3f749106cc8608cebdda0e37532eb5a29e
JuliaRinzilotti <rintz@posteo.de>
2021-11-02 18:38:03 +0100

    Version_1

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+let name = document.getElementById("nameJs");
+let email = document.getElementById("emailJs");
+let message = document.getElementById("messageJs");
+
+
+function btn() {
+    console.log(name.value)
+    console.log(email.value)
+    console.log(message.value)
+}
